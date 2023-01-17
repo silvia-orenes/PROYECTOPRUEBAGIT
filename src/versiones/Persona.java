a19d9c6866a377ee4ab9f42b65e3222de735e27e
silvia.orenes <silvia.orenes@u-tad.com>
2023-01-17 20:50:14 +0100

    Creacion de getter y setter de Persona

--- src/versiones/Persona.java
+++ src/versiones/Persona.java
@@ -13,6 +13,38 @@ public class Persona {
 		this.apellidos = apellidos;
 		this.dni = dni;
 	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+
+	public String getDni() {
+		return dni;
+	}
+
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	
+	
 	
 	
 }
