5019e2a4a597a5df238bf2436eba0293e6ad834c
silvia.orenes <silvia.orenes@u-tad.com>
2023-01-17 20:36:25 +0100

    Creación de clase Persona

---
+++ src/versiones/Persona.java
@@ -0,0 +1,18 @@
+package versiones;
+
+public class Persona {
+
+	private String nombre;
+	private String apellidos;
+	private String dni;
+	
+	
+	public Persona(String nombre, String apellidos, String dni) {
+		super();
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.dni = dni;
+	}
+	
+	
+}
